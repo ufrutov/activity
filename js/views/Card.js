eb655c4a2508d84b14f69d88fca3b0bf03a1ce57
ufrutov <ufrutov.s@gmail.com>
2023-06-04 13:02:20 +0300

    Use local cards dict

--- js/views/Card.js
+++ js/views/Card.js
@@ -1,212 +1,221 @@
 var Card = Backbone.View.extend({
-	el: $('#play-block'),
-	template: 'card',
+	el: $("#play-block"),
+	template: "card",
 	progress: {},
-	params: 'card-params',
-	next: 'get-next',
+	params: "card-params",
+	next: "get-next",
 	run: false,
-	word: '',
+	word: "",
 	interval_status: 1,
 	current: {},
-	events: {
-		
-	},
+	events: {},
 
-	initialize: function(){
-		_.bindAll(this, 'render');
+	initialize: function () {
+		_.bindAll(this, "render");
 		this.render();
 	},
 
-	destroy: function() {
-		$(this.el).html('');
+	destroy: function () {
+		$(this.el).html("");
 	},
 
-	add_point: function(e) {
+	add_point: function (e) {
 		var me = this,
-			cnt = $(e.currentTarget).data('cnt'),
-			i = $(e.currentTarget).data('command');
-		App.settings.commands[i].set('count', cnt+=1);
-		$('.card-body > nav').html('<button type="button" class="btn btn-info" id="get-other" data-next="true">Выбрать следующую</button>');
+			cnt = $(e.currentTarget).data("cnt"),
+			i = $(e.currentTarget).data("command");
+		App.settings.commands[i].set("count", (cnt += 1));
+		$(".card-body > nav").html(
+			'<button type="button" class="btn btn-info" id="get-other" data-next="true">Выбрать следующую</button>'
+		);
 
 		App.set_cookie();
 	},
 
-	start: function() {
-		console.log('[Card] start');
+	start: function () {
+		console.log("[Card] start");
 		var me = this;
-		$('#close-play').hide();
+		$("#close-play").hide();
 		// Encode word
 
-		$('#word').html( me.encode_word($('#word').html()) );
-		$('#word').addClass('encoded');
+		$("#word").html(me.encode_word($("#word").html()));
+		$("#word").addClass("encoded");
 
-		$.get("js/templates/progress.html", function(template){
-	      	$('.card-body > nav').html(template);
-	      	var i = 1;
-	      	me.progress = setInterval(function() {
-	      		me.interval(i);
-	      		i++;
-	      	}, 1000);
-	      	me.run = true;
-	    });
-	},
-
-	stop: function() {
-		var me = this;
-		if( me.run ) {
-			clearInterval(me.progress);
-			me.run = false;
-			$('.progress .progress-bar').addClass('progress-bar-warning');
-			$('.progress-buttons').show();
-		} else {
-			me.progress = setInterval(function() {
-				me.interval(me.interval_status);
-	      		me.interval_status++;
+		$.get("js/templates/progress.html", function (template) {
+			$(".card-body > nav").html(template);
+			var i = 1;
+			me.progress = setInterval(function () {
+				me.interval(i);
+				i++;
 			}, 1000);
 			me.run = true;
-			$('.progress .progress-bar').removeClass('progress-bar-warning');
-			$('.progress-buttons').hide();
+		});
+	},
+
+	stop: function () {
+		var me = this;
+		if (me.run) {
+			clearInterval(me.progress);
+			me.run = false;
+			$(".progress .progress-bar").addClass("progress-bar-warning");
+			$(".progress-buttons").show();
+		} else {
+			me.progress = setInterval(function () {
+				me.interval(me.interval_status);
+				me.interval_status++;
+			}, 1000);
+			me.run = true;
+			$(".progress .progress-bar").removeClass("progress-bar-warning");
+			$(".progress-buttons").hide();
 		}
 	},
 
-	interval: function(i) {
+	interval: function (i) {
 		var me = this,
 			time = App.settings[this.current],
-			limit = Math.floor((time*80)/100);
+			limit = Math.floor((time * 80) / 100);
 		me.interval_status = i;
-		if(i > time) {
+		if (i > time) {
 			try {
-				if( App.settings.sound )
-					$('#beep')[0].play();
-			} catch(e) {
-				console.error('[E] Error on play audio: ', e);
+				if (App.settings.sound) $("#beep")[0].play();
+			} catch (e) {
+				console.error("[E] Error on play audio: ", e);
 			}
-  			clearInterval(me.progress);
-  			$('#close-play').show();
-  			$.get("js/templates/" + me.next + ".html", function(template){
-  				var btn = _.template(template);
-  				$('.card-body > nav').html('');
-  				$.each(App.settings.commands, function(i, v) {
-  					$('.card-body > nav').append(btn({
-  						name : v.get('name'),
-  						count: v.get('count'),
-  						i    : i
-  					}));
-  				});
-  				$('.card-body > nav').append('<div class="no-one"><button type="button" id="get-other" class="btn btn-default btn-xs non-command" data-next="true">Выбрать другую карточку</button></div>');
-  			});
-  		}
-  		if(i == limit)
-  			$('.progress .progress-bar').addClass('progress-bar-danger').removeClass('progress-bar-success');
+			clearInterval(me.progress);
+			$("#close-play").show();
+			$.get("js/templates/" + me.next + ".html", function (template) {
+				var btn = _.template(template);
+				$(".card-body > nav").html("");
+				$.each(App.settings.commands, function (i, v) {
+					$(".card-body > nav").append(
+						btn({
+							name: v.get("name"),
+							count: v.get("count"),
+							i: i,
+						})
+					);
+				});
+				$(".card-body > nav").append(
+					'<div class="no-one"><button type="button" id="get-other" class="btn btn-default btn-xs non-command" data-next="true">Выбрать другую карточку</button></div>'
+				);
+			});
+		}
+		if (i == limit)
+			$(".progress .progress-bar")
+				.addClass("progress-bar-danger")
+				.removeClass("progress-bar-success");
 
-  		$('.progress .progress-bar').css('width', (i*100/time)+'%').attr('aria-valuenow', i);
+		$(".progress .progress-bar")
+			.css("width", (i * 100) / time + "%")
+			.attr("aria-valuenow", i);
 	},
 
-	show_count: function() {
+	show_count: function () {
 		var me = this;
 
 		me.stop();
 		clearInterval(me.progress);
 
-		$('#close-play').show();
-		
-		$.get("js/templates/" + me.next + ".html", function(template){
+		$("#close-play").show();
+
+		$.get("js/templates/" + me.next + ".html", function (template) {
 			var btn = _.template(template);
-			$('.card-body > nav').html('');
-			$.each(App.settings.commands, function(i, v) {
-				$('.card-body > nav').append(btn({
-					name : v.get('name'),
-					count: v.get('count'),
-					i    : i
-				}));
+			$(".card-body > nav").html("");
+			$.each(App.settings.commands, function (i, v) {
+				$(".card-body > nav").append(
+					btn({
+						name: v.get("name"),
+						count: v.get("count"),
+						i: i,
+					})
+				);
 			});
-			$('.card-body > nav').append('<div class="no-one"><button type="button" id="get-other" class="btn btn-default btn-xs non-command" data-next="true">Выбрать другую карточку</button></div>');
+			$(".card-body > nav").append(
+				'<div class="no-one"><button type="button" id="get-other" class="btn btn-default btn-xs non-command" data-next="true">Выбрать другую карточку</button></div>'
+			);
 		});
 	},
 
-	get_card: function(btn) {
-		var me = this,
-			url = window.location.href;
-		$('#close-play').show();
-		$.ajax({
-			url: url + 'db/index.php',
-			data: {action: 'get_card'},
-			type: 'post',
-			success: function(output) {
-				var data = $.parseJSON(output)[0];
+	get_card: function (btn) {
+		if (!activity_words && activity_words.length == 0) {
+			console.error("[E] Activity Words data is missing! Check db/cards.json file.");
+			return;
+		}
 
-				$('#card-id').html(data.id);
-				$('#word').html(data.word);
-				$('#word').removeClass('encoded');
-				me.word = data.word;
+		var index = Math.floor(Math.random() * activity_words.length);
+		var me = this;
 
-				if(btn) {
-					$.get("js/templates/card-buttons.html", function(template){
-						$('.card-body > nav').html(template);
-					});
-				}
+		$("#close-play").show();
 
-				$.get("js/templates/" + me.params + ".html", function(template){
-			      	var parameters = _.template(template),
-			      		common = Math.floor(Math.random() * 3) + 1, // 1 to 3
-			      		params = [0, 0, 0];
-			      	params[(Math.floor(Math.random() * 3) + 1) - 1] = 1;
+		var data = activity_words[index];
 
-			      	$('.parameters').html(parameters({
-			      		common: common == 1 ? 1 : 0,
-			      		draw  : params[0],
-						say   : params[1],
-						show  : params[2]
-			      	}));
-			      	switch (params.indexOf(1)) {
-			      		case 0:
-			      			me.current = 'draw';
-			      			break;
-			      		case 1:
-			      			me.current = 'say';
-			      			break;
-			      		case 2:
-			      			me.current = 'show';
-			      			break;
-			      	}
-			    });
+		$("#card-id").html(data.id);
+		$("#word").html(data.word);
+		$("#word").removeClass("encoded");
+		me.word = data.word;
+
+		if (btn) {
+			$.get("js/templates/card-buttons.html", function (template) {
+				$(".card-body > nav").html(template);
+			});
+		}
+
+		$.get("js/templates/" + me.params + ".html", function (template) {
+			var parameters = _.template(template),
+				common = Math.floor(Math.random() * 3) + 1, // 1 to 3
+				params = [0, 0, 0];
+			params[Math.floor(Math.random() * 3) + 1 - 1] = 1;
+
+			$(".parameters").html(
+				parameters({
+					common: common == 1 ? 1 : 0,
+					draw: params[0],
+					say: params[1],
+					show: params[2],
+				})
+			);
+			switch (params.indexOf(1)) {
+				case 0:
+					me.current = "draw";
+					break;
+				case 1:
+					me.current = "say";
+					break;
+				case 2:
+					me.current = "show";
+					break;
 			}
 		});
 
 		return false;
 	},
 
-	render: function(e) {
+	render: function (e) {
 		var me = this;
-		me.el = $('#play-block');
-		$.get("js/templates/" + me.template + ".html", function(template){
-	      	$(me.el).html(template);
-	      	me.get_card(false);
-	    });
-	    return false;
-	},
-
-	encode_word: function(w) {
-		var out = [];
-		$.each(w.split(''), function(i, v) {
-			if(/[\wа-я]+/ig.test(v))
-				out.push('*');
-			else
-				out.push(v);
+		me.el = $("#play-block");
+		$.get("js/templates/" + me.template + ".html", function (template) {
+			$(me.el).html(template);
+			me.get_card(false);
 		});
-		return out.join('');
+		return false;
 	},
 
-	decode_word: function(e) {
+	encode_word: function (w) {
+		var out = [];
+		$.each(w.split(""), function (i, v) {
+			if (/[\wа-я]+/gi.test(v)) out.push("*");
+			else out.push(v);
+		});
+		return out.join("");
+	},
+
+	decode_word: function (e) {
 		var me = this;
-		if(e) {
-			$('#word').html( me.word );
-			$('#word').removeClass('encoded');
+		if (e) {
+			$("#word").html(me.word);
+			$("#word").removeClass("encoded");
 		} else {
-			$('#word').html( me.encode_word($('#word').html()) );
-			$('#word').addClass('encoded');
+			$("#word").html(me.encode_word($("#word").html()));
+			$("#word").addClass("encoded");
 		}
-
-	}
-
+	},
 });
--- js/views/CardListView.js
+++ js/views/CardListView.js
@@ -1,133 +1,136 @@
 var CardListView = Backbone.View.extend({
-	el: $('#card-list'),
-	template: 'cards-block',
-	list_template: 'cards-list',
-	list_template_content: 'cards-list-content',
+	el: $("#card-list"),
+	template: "cards-block",
+	list_template: "cards-list",
+	list_template_content: "cards-list-content",
 	autocomplete: [],
 
 	events: {
-		'click #close-cards': 'destroy',
-		'click #add-card'   : 'addCard',
-		'click .list'       : 'edit_card',
-		'click #cancel'		: 'cancel_edit',
-		'click #save'		: 'save_edit',
-		'click #delete'		: 'delete_card',
-		'focus #new-word'   : 'focus_word',
-		'keyup #new-word'   : 'enter_word'
+		"click #close-cards": "destroy",
+		"click #add-card": "addCard",
+		"click .list": "edit_card",
+		"click #cancel": "cancel_edit",
+		"click #save": "save_edit",
+		"click #delete": "delete_card",
+		"focus #new-word": "focus_word",
+		"keyup #new-word": "enter_word",
 	},
 
-	initialize: function(){
-		_.bindAll(this, 'render');
-		$('#new-word').on('change', this.input_word, this);
+	initialize: function () {
+		_.bindAll(this, "render");
+		$("#new-word").on("change", this.input_word, this);
 		this.render();
 	},
 
-	destroy: function() {
-		$(this.el).html('');
+	destroy: function () {
+		$(this.el).html("");
 	},
 
-	edit_card: function(e) {
+	edit_card: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
 
 		var me = this,
 			url = window.location.href,
-			id = $(e.currentTarget).data('id');
+			id = $(e.currentTarget).data("id");
 		$.ajax({
-				url: url + 'db/index.php',
-				data: {
-					action: 'get_card_by_id',
-					id: id
-				},
-				type: 'post',
-				success: function(output) {
-					var data = $.parseJSON(output)[0];
-					$.get("js/templates/edit-modal.html", function(template){
-						var temp = _.template(template);
-						
-						$('#modal').html(temp({
-							id: data['id'],
-							word: data['word'],
-							draw: data['draw'],
-							say: data['say'],
-							show: data['show']
-						}));
-					});
+			url: url + "db/index.php",
+			data: {
+				action: "get_card_by_id",
+				id: id,
+			},
+			type: "post",
+			success: function (output) {
+				var data = $.parseJSON(output)[0];
+				$.get("js/templates/edit-modal.html", function (template) {
+					var temp = _.template(template);
 
-				}
-			});
+					$("#modal").html(
+						temp({
+							id: data["id"],
+							word: data["word"],
+							draw: data["draw"],
+							say: data["say"],
+							show: data["show"],
+						})
+					);
+				});
+			},
+		});
 
 		return false;
 	},
 
-	save_edit: function(e) {
+	save_edit: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
-		
+
 		var me = this,
 			url = window.location.href,
-			id = $(e.currentTarget).data('id'),
-			word = $('.word-input').val(),
-			draw = ($('.modale [name=draw]').is(':checked')) ? 1 : 0,
-			say = ($('.modale [name=say]').is(':checked')) ? 1 : 0,
-			show = ($('.modale [name=show]').is(':checked')) ? 1 : 0;
+			id = $(e.currentTarget).data("id"),
+			word = $(".word-input").val(),
+			draw = $(".modale [name=draw]").is(":checked") ? 1 : 0,
+			say = $(".modale [name=say]").is(":checked") ? 1 : 0,
+			show = $(".modale [name=show]").is(":checked") ? 1 : 0;
 
-		if( (word.length != 0) && $('.modale [type=checkbox]').is(':checked') ) {
+		if (word.length != 0 && $(".modale [type=checkbox]").is(":checked")) {
 			$.ajax({
-				url: url + 'db/index.php',
+				url: url + "db/index.php",
 				data: {
-					action: 'edit_card',
+					action: "edit_card",
 					id: id,
 					word: word,
 					draw: draw,
-					say : say,
-					show: show
+					say: say,
+					show: show,
 				},
-				type: 'post',
-				success: function(output) {
+				type: "post",
+				success: function (output) {
 					var data = $.parseJSON(output);
-					$('#modal').html('');
-					$.get("js/templates/" + me.list_template_content + ".html", function(list){
+					$("#modal").html("");
+					$.get("js/templates/" + me.list_template_content + ".html", function (list) {
 						var template = _.template(list),
-							li = $('.list[data-id='+data['id']+']');
-						li.html(template({
-							word: data['word'],
-							draw: data['draw'],
-							say : data['say'],
-							show: data['show']
-						}));
+							li = $(".list[data-id=" + data["id"] + "]");
+						li.html(
+							template({
+								word: data["word"],
+								draw: data["draw"],
+								say: data["say"],
+								show: data["show"],
+							})
+						);
 					});
-				}
+				},
 			});
 		}
 
 		return false;
 	},
 
-	cancel_edit: function() {
-		$('#modal').html('');
+	cancel_edit: function () {
+		$("#modal").html("");
 	},
 
-	delete_card: function(e) {
+	delete_card: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
 
 		var me = this,
 			url = window.location.href,
-			id = $(e.currentTarget).data('id');
-		if (window.confirm("Удалить карточку?")) { 
+			id = $(e.currentTarget).data("id");
+		if (window.confirm("Удалить карточку?")) {
 			$.ajax({
-				url: url + 'db/index.php',
+				url: url + "db/index.php",
 				data: {
-					action: 'delete_card',
-					id: id
+					action: "delete_card",
+					id: id,
 				},
-				type: 'post',
-				success: function(output) {
+				type: "post",
+				success: function (output) {
 					var data = $.parseJSON(output);
-					$('#modal').html('');
-					$('.list[data-id='+data['id']+']')[0].remove();
-				}
+					$("#modal").html("");
+					$(".list[data-id=" + data["id"] + "]")[0].remove();
+				},
 			});
 		}
 
@@ -135,51 +138,52 @@ var CardListView = Backbone.View.extend({
 	},
 
 	// Add new card to data base
-	addCard: function(e) {
+	addCard: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
-		
+
 		var me = this,
 			url = window.location.href,
-			params = $('#card-form').serializeArray();
+			params = $("#card-form").serializeArray();
 
-		if(params[0].value.length != 0 && $('#card-form [type=checkbox]').is(':checked') ) {
-			$('#cards-count').html('<img src="img/loading.gif"/>');
+		if (params[0].value.length != 0 && $("#card-form [type=checkbox]").is(":checked")) {
+			$("#cards-count").html('<img src="img/loading.gif"/>');
 			$.ajax({
-				url: url + 'db/index.php',
+				url: url + "db/index.php",
 				data: {
-					action: 'add_card',
-					word  : $('#card-form [name=word]').val(),
-					draw  : ($('#card-form [name=draw]').is(':checked')) ? 1 : 0,
-					say   : ($('#card-form [name=say]').is(':checked')) ? 1 : 0,
-					show  : ($('#card-form [name=show]').is(':checked')) ? 1 : 0
+					action: "add_card",
+					word: $("#card-form [name=word]").val(),
+					draw: $("#card-form [name=draw]").is(":checked") ? 1 : 0,
+					say: $("#card-form [name=say]").is(":checked") ? 1 : 0,
+					show: $("#card-form [name=show]").is(":checked") ? 1 : 0,
 				},
-				type: 'post',
-				success: function(output) {
-					
+				type: "post",
+				success: function (output) {
 					var data = $.parseJSON(output);
 
-					$('#cards-count').html(data['count']);
-					$('#card-form')[0].reset();
+					$("#cards-count").html(data["count"]);
+					$("#card-form")[0].reset();
 
-					$.get("js/templates/" + me.list_template + ".html", function(list){
+					$.get("js/templates/" + me.list_template + ".html", function (list) {
 						var template = _.template(list);
-						$('#card-listing > ul').prepend(template({
-							id: data['id'],
-							word: data['word'],
-							draw: data['draw'],
-							say : data['say'],
-							show: data['show']
-						}));
+						$("#card-listing > ul").prepend(
+							template({
+								id: data["id"],
+								word: data["word"],
+								draw: data["draw"],
+								say: data["say"],
+								show: data["show"],
+							})
+						);
 					});
-				}
+				},
 			});
 		}
 
 		return false;
 	},
 
-	focus_word: function(e) {
+	focus_word: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
 
@@ -187,83 +191,76 @@ var CardListView = Backbone.View.extend({
 			url = window.location.href;
 
 		$.ajax({
-				url: url + 'db/index.php',
-				data: {action: 'get_cards'},
-				type: 'post',
-				success: function(output) {
-					var data = $.parseJSON(output);
-					
-					me.autocomplete = [];
-					
-					$.each(data, function(i, v) {
-						me.autocomplete.push(v.word);
-					});
-				}
-			});
+			url: url + "db/index.php",
+			data: { action: "get_cards" },
+			type: "post",
+			success: function (output) {
+				var data = $.parseJSON(output);
+
+				me.autocomplete = [];
+
+				$.each(data, function (i, v) {
+					me.autocomplete.push(v.word);
+				});
+			},
+		});
 
 		return false;
 	},
 
-	enter_word: function(e) {
+	enter_word: function (e) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
-		
+
 		var me = this,
 			input = $(e.currentTarget).val(),
 			complete = [];
-		
-		$('#autocomplete').html('');
 
-		if ( input.length > 2 ) {
-			
-			$.each(me.autocomplete, function(i, v) {
-				if( v.toLowerCase().match(input.toLowerCase()) )
-					complete.push(v);
+		$("#autocomplete").html("");
+
+		if (input.length > 2) {
+			$.each(me.autocomplete, function (i, v) {
+				if (v.toLowerCase().match(input.toLowerCase())) complete.push(v);
 			});
 
-			if ( complete.length > 0 )
-				$('#autocomplete').html('<ul></ul>');
+			if (complete.length > 0) $("#autocomplete").html("<ul></ul>");
 
-			$.each(complete, function(i, v) {
-				$('#autocomplete ul').append('<li>'+v+'</li>');
+			$.each(complete, function (i, v) {
+				$("#autocomplete ul").append("<li>" + v + "</li>");
 			});
-
 		}
 
 		return false;
 	},
 
-	render: function() {
-		var me = this,
-			url = window.location.href;
-			
-		var html = $.get("js/templates/" + me.template + ".html", function(template){
-	      	$(me.el).html(template);
+	render: function () {
+		if (!activity_words && activity_words.length == 0) {
+			console.error("[E] Activity Words data is missing! Check db/cards.json file.");
+			return;
+		}
 
-	      	$.ajax({
-				url: url + 'db/index.php',
-				data: {action: 'get_cards'},
-				type: 'post',
-				success: function(output) {
-					var data = $.parseJSON(output);
-					$('#cards-count').html(data.length);
-					$('#card-listing').html('<ul></ul>');
-					$.get("js/templates/" + me.list_template + ".html", function(list){
+		var me = this;
 
-						var template = _.template(list);
-						$.each(data.reverse(), function(i) {
-							$('#card-listing > ul').append(template({
-								id  : data[i]['id'],
-								word: data[i]['word'],
-								draw: data[i]['draw'],
-								say : data[i]['say'],
-								show: data[i]['show']
-							}));
-						});
-					});
-				}
+		$.get("js/templates/" + me.template + ".html", function (template) {
+			$(me.el).html(template);
+
+			var data = activity_words;
+			$("#cards-count").html(data.length);
+			$("#card-listing").html("<ul></ul>");
+			$.get("js/templates/" + me.list_template + ".html", function (list) {
+				var template = _.template(list);
+				$.each(data.reverse(), function (i) {
+					$("#card-listing > ul").append(
+						template({
+							id: data[i]["id"],
+							word: data[i]["word"],
+							draw: data[i]["draw"],
+							say: data[i]["say"],
+							show: data[i]["show"],
+						})
+					);
+				});
 			});
-	    });
-	}
-
+		});
+	},
 });
